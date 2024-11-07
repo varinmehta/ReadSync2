b2b043ec4815129dea7ba083d9e0023a9d1d7992
varinmehta <mehtavarin2004@gmail.com>
2024-11-07 09:17:31 +0530

    commit

--- server/app.js
+++ server/app.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const user = require("../routes/user");
+const user = require("./routes/user");
 const cors = require("cors");
 require("dotenv").config();
-const connectDB = require("../db/connection");
+const connectDB = require("./db/connection");
 
 const app = express();
 app.use(cookieParser());
+connectDB(process.env.MONGO_URL);
+app.listen(process.env.PORT || 8000);
 app.use(
     cors({
-        origin: "https:read-sync2-frotend.vercel.app",
+        origin: "https:read-sync2-frontend.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
 );
 
-connectDB(process.env.MONGO_URL);
-app.listen(process.env.PORT || 8000);
+// app.use(
+//     cors({
+//         origin: "http://localhost:5173",
+//         credentials: true,
+//     })
+// );
 
 //Middleware
 
